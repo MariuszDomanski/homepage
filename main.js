ce95afc67c16b56e097fdb7170955263e8c468a3
Mariusz Domanski <domanski.m.p@gmail.com>
2020-05-12 15:19:20 +0200

    prettier code format changes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const name = 'Mariusz';
-const age = 100; 
+const name = "Mariusz";
+const age = 100;
 console.log(name);
 console.log(age);
 
 console.log(`Czołgiem, nazywam się ${name} i mam ${age} lat.`);
-
-    
