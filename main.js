706567ec149d143233318ad6fd30ef8f5b0630e0
Mariusz Domanski <domanski.m.p@gmail.com>
2020-06-01 18:15:10 +0200

    next BEM and document query selector experiments

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ const age = 100;
 console.log(name);
 console.log(age);
 
-console.log(`Czołgiem, nazywam się ${name} i mam ${age} lat.`);
+console.log(
+    `Czołgiem, nazywam się ${name} i mam ${age} lat.`
+    );
+
+const heading = document.querySelector(' .resume__header--js ');
+console.log(heading.innerHTML);
+heading.innerHTML = `joł joł joł`;
+console.log(heading.innerHTML);
+
+const emptyParagraph = document.querySelector('.resume__paragraph--js')
+emptyParagraph.innerHTML = 'Javascript jest super. Uzupełniam nim treść '
